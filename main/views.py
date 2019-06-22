a4f2b38cd1ddebab84132af8474f6dac35f42553
Mariam Ali <mariamqali13@gmail.com>
2019-06-23 00:26:09 +0300

    pagination

--- main/views.py
+++ main/views.py
@@ -2,10 +2,29 @@ from django.shortcuts import render, redirect
 from .models import Article, Contribution, Change
 from .forms import ArticleForm, ContributeArticleForm 
 import difflib
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def articles_list(request):
+	articles = Article.objects.all()
+
+	query = request.GET.get("q")
+	if query:
+		articles = articles.filter(title__icontains=query)
+
+	paginator = Paginator(articles, 3) # Show 3 articles per page
+	page_number = request.GET.get('page')
+
+	try:
+		objects = paginator.page(page_number)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		objects = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		objects = paginator.page(paginator.num_pages)
+
 	context = {
-		"articles" : Article.objects.all(),
+		"articles" : objects,
 	}
 	return render(request, "articles_list.html", context)
 
